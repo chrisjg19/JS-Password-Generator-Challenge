b69a9f490c166a19ecfb02ddc42c2c422043f4c8
christopher gonzalez <christopher.gonzalez98@gmail.com>
2022-12-11 14:59:39 -0600

    added some more comments and even added a return 'test' to see if showed and it did

--- Develop/script.js
+++ Develop/script.js
@@ -5,9 +5,11 @@ var generateBtn = document.querySelector("#generate");
 
 /*so within this function I should probably add all the asked criteria like the special character, uppercase, lowercase and numbers.But how do I do that? */
 function generatePassword() {
+/*definetely will need a return command since i am tryin to bring something back, also test to see if a return.  */
 
+return "test"
 }
-/* so based on this function it seems everything is already set up */
+/* so based on this function it seems everything is already set up, what doesnt seem set up is the generatePassword function since there is nothing writtern for it */
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
