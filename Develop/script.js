a7c254caaefc5fdf217a2015dd401cb37a45c19b
christopher gonzalez <christopher.gonzalez98@gmail.com>
2022-12-11 13:56:40 -0600

    added some comments to help understand what to do

--- Develop/script.js
+++ Develop/script.js
@@ -2,9 +2,12 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+/*so within this function I should probably add all the asked criteria like the special character, uppercase, lowercase and numbers.But how do I do that? */
 function generatePassword() {
-  
+
 }
+/* so based on this function it seems everything is already set up */
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
