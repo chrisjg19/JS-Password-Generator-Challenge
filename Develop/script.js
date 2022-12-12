927f24fa77730c8a2ada62da05ae5a0c35cd67ba
christopher gonzalez <christopher.gonzalez98@gmail.com>
2022-12-12 17:47:08 -0600

    generator was not working properly, so I removed all the comas and spaces in the var defining the characters that were going to be included and this fixed my issue!

--- Develop/script.js
+++ Develop/script.js
@@ -8,10 +8,10 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
 
   /*definetely will need a return command since i am tryin to bring something back, also test to see if a return happens.  */
-var lowerCase = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
-var upperCase = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
-var numbers = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
-var specialChar = " !, #, $, %, &, ', (, ), *, +, ,, -, ., /, :, ;, <, =, >, ?, @, [, \, ], ^, _, `, {, |, }, ~";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 // Alert windows could be set up to prompt if any of these criterias want to be used. Using If or else statements?
  var passLength = prompt("Please input desired password length, between 8 to 128 characters")
