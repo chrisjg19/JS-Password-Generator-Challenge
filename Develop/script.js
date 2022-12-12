6ff40fde22d863948f4b1b36a97458d043fed379
christopher gonzalez <christopher.gonzalez98@gmail.com>
2022-12-11 18:29:53 -0600

    added an initial prompt to ask for the number of characters between 8 to 128 for the password'

--- Develop/script.js
+++ Develop/script.js
@@ -7,13 +7,17 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-  /*definetely will need a return command since i am tryin to bring something back, also test to see if a return.  */
+  /*definetely will need a return command since i am tryin to bring something back, also test to see if a return happens.  */
 var lowerCase = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"
 var upperCase = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"
 var numbers = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0"
 var specialChar = " !, #, $, %, &, ', (, ), *, +, ,, -, ., /, :, ;, <, =, >, ?, @, [, \, ], ^, _, `, {, |, }, ~"
 
-return "test"
+// Alert windows could be set up to prompt if any of these criterias want to be used. Using If or else statements?
+ var passLength = prompt("Please input desired password length, between 8 to 128 characters")
+
+//inside the return i need to place the final product, I think.
+return 
 }
 /* so based on this function it seems everything is already set up, what doesnt seem set up is the generatePassword function since there is nothing writtern for it */
 function writePassword() {
